444ef6f2af9ee44dbcbea1d0d2699816ef92b314
CbnMarian <m.marian03@yahoo.com>
2024-02-12 13:55:30 +0200

    new comp

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+import StarRating from "./starRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <StarRating maxRating={5} defaultRating={3} />
   </React.StrictMode>
 );
